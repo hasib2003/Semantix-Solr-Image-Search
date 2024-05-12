b95d53f787bf9c249a6850c60a372efc98af5761
Hasib Aslam <you@example.com>
2024-05-12 18:59:54 +0500

    Inference and Query handler working

--- QueryHandler.py
+++ QueryHandler.py
@@ -4,8 +4,42 @@ Provides methods for recieving the users query and preprocessing it, communicati
 
 
 from vectorizer import Vectorizer
+from inference import Inference
+
+from PIL import Image
+import torch
+import os
 
 class QueryHandler():
     def __init__(self) -> None:
+
+        self.vectorizer = Vectorizer() # for preprocessing the query
+        self.infer = Inference("http://localhost:8983/solr","image_vector_collection","")
+
+    def query_by_image(self,image_path):
+
+        assert os.path.isfile(image_path),f"No file found at {image_path}"
         
-        self.vectorizer = Vectorizer()
+        img = Image.open(image_path).convert("L")
+        img_vector = torch.squeeze(self.vectorizer.vectorize_img(img)).tolist()
+        img_best_matches = self.infer.query_images(img_vector)
+        # desc_best_matcher = self.infer.query_descriptions(img)
+        print(img_best_matches)
+
+    def query_by_text(self,text):
+        
+        assert type(text) is str ,f"Input should be of type str"
+        query_vector = torch.squeeze(self.vectorizer.vectorize_text(text)).tolist()
+        best_matches = self.infer.query_images(query_vector)
+        print(best_matches)
+
+
+
+handler = QueryHandler()
+# handler.query_by_image
+
+
+
+
+
+        
--- inference.py
+++ inference.py
@@ -4,36 +4,89 @@ import torch
 import pysolr
 
 
-vectorizer = Vectorizer()
-
-text = "cars"
-text_vector = torch.squeeze(vectorizer.vectorize_text(text)).to(torch.float16)
-print(text_vector.size())
-text_vector = text_vector.tolist()
 
 
-import pysolr
+class Inference():
+    def __init__(self,solr_url , image_collection,description_collection) -> None:
+        # self.image_collection = image_collection
+        # self.descripton_collection = description_collection;
+        # self.solr_url = solr_url
 
-# Connect to Solr
-solr = pysolr.Solr('http://localhost:8983/solr/Semantic_Search', always_commit=True)
-# solr.delete(q='*:*')
-# Define your query parameters
-# query = {
- 
-#     'fq':  f'{"{!func}"}vector(description_vector,{text_vector} )', # Define your vector field and query
-#     'sort': 'score desc',  # Sort by score
-# }
-
-# Execute the query
-# # results = solr.search(q="*:*")
-q = f'{{!knn f=image_vector topK=10}}{text_vector}'
-print("->",q)
-results = solr.search(q=q)
+        self.vectorizer = Vectorizer()
+        self.solr_images_instance = pysolr.Solr(f"{solr_url}/{image_collection}", always_commit=True)
+        self.solr_description_instance = pysolr.Solr(f"{solr_url}/{description_collection}", always_commit=True)
 
 
-for result in results:
-    print(result)
-    print("\n\n")
+
+    def query_images(self,query_vector):
+
+        """
+        sends request to image_vector_collection, returns the list of paths of best matches
+        """
+        
+        assert type(query_vector) is list and len(query_vector)==512, "input should be the list of 512 dimension"
+        
+        q = f'{{!knn f=image_vector topK=10}}{query_vector}'
+        results = self.solr_images_instance.search(q)
+
+        img_list = []
+
+        for result in results:
+            img_list.append(result["image_path"])
+        
+
+        return img_list
+    
+    def query_descriptions(self,query_vector):
+
+        """
+        sends request to description_vector_collection, returns the list of paths of best matches
+        """
+        
+        assert type(query_vector) is list and len(query_vector)==512, "input should be the list of 512 dimension"
+        
+        q = f'{{!knn f=image_vector topK=10}}{query_vector}'
+        results = self.solr_description_instance.search(q)
+
+        img_list = []
+
+        for result in results:
+            img_list.append(result["image_path"])
+        
+
+        return list(set(img_list))
+        
+
+
+inferencer = Inference("http://localhost:8983/solr","image_vector_collection","")
+inferencer.query_images(input())
+
+
+
+
+# vectorizer = Vectorizer()
+
+# text = "a scene of children playing in ground"
+# text_vector = torch.squeeze(vectorizer.vectorize_text(text)).to(torch.float16)
+
+# print(text_vector.size())
+# text_vector = text_vector.tolist()
+
+
+
+# q = f'{{!knn f=image_vector topK=10}}{text_vector}'
+
+# results = .solr_images_instance.search(q)
+
+# # Connect to Solr
+
+
+
+
+# for result in results:
+#     print("\n\n")
+#     print(result)
+#     print("\n\n")
 
 
 
